b15dd04ac6033f40d47f9569b76d8348ba6c2f87
omarkhaled <omar.ceh007@gmail.com>
2022-06-08 14:24:16 +0300

    first commit

---
+++ reeltik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  reeltik
+//
+//  Created by Omar Khaled on 08/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
